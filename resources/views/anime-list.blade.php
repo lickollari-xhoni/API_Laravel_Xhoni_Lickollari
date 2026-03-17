fd4b9fec2315ec0d989667fff58678cd42977b98
xhoni lickollari <lickollarixhoni@yahoo.it>
2026-03-17 17:03:40 +0100

    aggiornata parte frontend anime-list e creata nuova pagina css

--- resources/views/anime-list.blade.php
+++ resources/views/anime-list.blade.php
@@ -1,13 +1,36 @@
 <x-layout>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-12 mb-4">
+                <h2 class="border-bottom pb-2">Esplora i Generi</h2>
+                <p class="text-muted">Seleziona una categoria per scoprire nuovi anime.</p>
+            </div>
+        </div>
 
-    <ul>
-        @foreach ($results['data'] as $result)
-            <li>
-                <a href="{{ route('rotta2', ['id' => $result['mal_id']]) }}">
-                    {{ $result['name'] }} -Anime Associati:{{ $result['count'] }}
-                </a>
-            </li>
-        @endforeach
-    </ul>
+        <div class="row g-4">
+            @foreach ($results['data'] as $result)
+                <div class="col-md-4 col-lg-3">
+                    <div class="card h-100 shadow-sm border-0">
+                        <div class="card-body d-flex flex-column justify-content-between">
+                            <div>
+                                <h5 class="card-title fw-bold text-dark">
+                                    {{ $result['name'] }}
+                                </h5>
+                                <p class="card-text text-muted small">
+                                    Anime associati: <span class="badge bg-info text-dark">{{ $result['count'] }}</span>
+                                </p>
+                            </div>
+                            
+                            <a href="{{ route('rotta2', ['id' => $result['mal_id']]) }}" 
+                               class="btn btn-dark w-100 mt-3 align-self-end">
+                                Vedi Anime
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 
+    
 </x-layout>
