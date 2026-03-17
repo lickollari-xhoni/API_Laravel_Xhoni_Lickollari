38d340a06e8bf1cff869971a46d8f3612228aa81
xhoni lickollari <lickollarixhoni@yahoo.it>
2026-03-17 12:12:51 +0100

    inserimento chiavi prese dal elenco_prodotti.json del progetto presto-phone

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'nome_prodotto',
+        'marca_prodotto',
+        'prezzo',
+        'descrizione',
+        'immagine_prodotto'
+    ];
 }
